a5bddd3ce2cf8a82b04609f57a12fa08af482ebf
wangzh <wangzh526@126.com>
2019-06-13 07:12:22 +0800

    add comment

--- app/src/main/java/com/wang/zh/andfixdemo/andfix/AndFixPatchManager.java
+++ app/src/main/java/com/wang/zh/andfixdemo/andfix/AndFixPatchManager.java
@@ -12,8 +12,9 @@ import java.io.IOException;
  */
 public class AndFixPatchManager {
 
-    private static AndFixPatchManager mInstance = null;
-    private static PatchManager mPatchManager;
+    // volatile禁止指令重排序
+    private static volatile AndFixPatchManager mInstance = null;
+    private PatchManager mPatchManager;
 
     public static AndFixPatchManager getInstance() {
         // 双检查机制
@@ -28,14 +29,21 @@ public class AndFixPatchManager {
         return mInstance;
     }
 
-    // 初始化AndFix
+    /**
+     * 初始化AndFix
+     */
     public void initPatchManager(Context context) {
         mPatchManager = new PatchManager(context);
         mPatchManager.init(Utils.getVersionName(context));//current version
+        // You should load patch as early as possible, generally,
+        // in the initialization phase of your application(such as Application.onCreate()).
         mPatchManager.loadPatch();
     }
 
-    // 加载patch文件
+    /**
+     * 加载patch文件
+     * @param path path of the patch file that was downloaded
+     */
     public void addPatch(String path) {
         try {
             if (mPatchManager != null)
